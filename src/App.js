a8e7557a9639f64a13741ff0f1cc5d57dd59f437
kiritoast <jinming625@gmail.com>
2023-02-14 21:22:29 -0800

    commit message

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import About from "./components/About";
+import Home from "./components/Home";
+import NavBar from "./components/NavBar";
+import SocialLinks from "./components/SocialLinks";
+
+function App() {
+  return (
+    <div>
+      <NavBar />
+      <Home/>
+      <SocialLinks/>
+      <About/>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/About.jsx
@@ -0,0 +1,25 @@
+import React from 'react'
+
+const About = () => {
+  return (
+    <div name="about className=">
+        <div>
+            <div>
+                <p>About
+                    
+                </p>
+            </div>
+            <p>
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. Repellendus 
+                illo assumenda sint mollitia eius tempore aut maiores ducimus fuga adipisci 
+                cupiditate, quas molestiae recusandae quidem aliquid laborum. Consequuntur, natus 
+                cumque?
+            </p>
+            <br />
+            <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Perferendis placeat iusto quos natus magni, a vel quod perspiciatis provident facere illum corrupti autem minus nemo sint velit totam et ab qui vero soluta. Doloribus, nihil ea. Totam, accusantium. Ea quis impedit nulla rerum asperiores? Ullam temporibus quam assumenda vel provident!</p>
+        </div>
+    </div> 
+  )
+}
+
+export default About
